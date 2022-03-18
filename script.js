aa9c717cb908565798a6aa627894c719377a4f21
TR742 <Tyrich1442@gmail.com>
2022-03-17 22:08:16 -0700

    Added two more prompts

--- script.js
+++ script.js
@@ -20,6 +20,22 @@ let generatePassword = function() {
    let userInput = prompt("How long would you like the password to be?\n\nChoose a number between 8 to 128.");
   if (userInput < 8 || userInput > 128) {
     confirm("Invalid option, please choose between 8 and 128");
-    generatePassword();
+    return; 
   }
+
+  let userInputNumbers = prompt("Would you like numbers in the password?");
+  if (userInputNumbers === "yes") {
+    let index = Math.floor(Math.random() * numbers.length);
+    let compNumber = numbers[index];
+  } else {
+    compNumber = "";
+  };
+
+  let userInputSpecial = prompt("Would you like special characters in the password?");
+  if (userInputSpecial === "yes") {
+    let index = Math.floor(Math.random() * special.length);
+    let compSpecial = special[index];
+  } else {
+    compSpecial = "";
+  };
 }
