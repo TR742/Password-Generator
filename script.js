31495c8210335f335289c6455406df271de3959b
TR742 <Tyrich1442@gmail.com>
2022-03-17 21:21:41 -0700

    Added arrays and first prompt

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
+// Arrays
+let letters = "abcdefghijklmnopqrstuvwxyz".split("");
+let numbers = "123456789".split("");
+let special = "!@#$%^&*?".split("");
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
   passwordText.value = password;
-
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+let generatePassword = function() {
+   let userInput = prompt("How long would you like the password to be?\n\nChoose a number between 8 to 128.");
+  if (userInput < 8 || userInput > 128) {
+    confirm("Invalid option, please choose between 8 and 128");
+    generatePassword();
+  }
+}
