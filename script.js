f23eb22986bcf4c589bf0ffc4be48ead0b1ba4b6
TR742 <Tyrich1442@gmail.com>
2022-03-21 23:30:33 -0700

    added conditionals to change option set

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let letters = "abcdefghijklmnopqrstuvwxyz";
 let numbers = "123456789";
 let special = "!@#$%^&*?";
-let options = "";
+let options = letters;
 
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
@@ -20,8 +20,8 @@ generateBtn.addEventListener("click", writePassword);
 
 // prompts to generate password
 function generatePassword() {
-   let userInput = prompt("How long would you like the password to be?\n\nChoose a number between 8 to 128.");
-  if (userInput < 8 || userInput > 128) {
+   let length = prompt("How long would you like the password to be?\n\nChoose a number between 8 to 128.");
+  if (length < 8 || length > 128) {
     confirm("Invalid option, please choose between 8 and 128");
     return; 
   };
@@ -31,4 +31,19 @@ function generatePassword() {
   let addNumbers = confirm("Numeric values\n\nWould you like to add numeric values to the password?");
   let addSpecials = confirm("Special characters\n\nWould you like to add special characters to the password?");
   let makeUpperCase = confirm("Capital letters\n\nWould you like all the letters in the password to be uppercase?");
+
+  if (addNumbers) {
+    options += numbers;
+    console.log('adding numbers to options', options);
+  };
+
+  if (addSpecials) {
+    options += special;
+    console.log('addingi specials to options', options);
+  };
+
+  if (makeUpperCase) {
+    options = options.toUpperCase();
+    console.log('making letters uppercase', options);
+  };
 };
